39934a3a6fc39a02cfba1e0349f1bd652009665a
bunny1v <tom7san7@gmail.com>
2024-11-02 16:30:07 +0530

    photos updated

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4174292512"
+    serviceWorkerVersion: "3582814569"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "97ed99539182e959249d580eb1320519",
-"assets/AssetManifest.bin.json": "e43b03f30842492df3ff74c0243d9d3f",
-"assets/AssetManifest.json": "c02f6bfc3d0a97d6741de59893f11836",
+const RESOURCES = {"assets/AssetManifest.bin": "a85bac27a8292c2fcfdd44d7b54d9646",
+"assets/AssetManifest.bin.json": "35966d1f0f8b7d2c4e772811989e014b",
+"assets/AssetManifest.json": "c8255c864e177c4669936fcc1003824f",
 "assets/assets/DJI_0744.JPG": "7f37f70f135beea2a163a8b33554a14b",
 "assets/assets/DJI_0745.JPG": "915b5eff5b578dd98aedb7557c9d0cd9",
-"assets/assets/DJI_0746.JPG": "adad84e8766e6b1a17b82efa16784438",
-"assets/assets/DJI_0748.JPG": "c35798f23fb5545e9679df30272d7e14",
 "assets/assets/image.png": "e03f41ad0830bc75e1fd5cb2834eb333",
+"assets/assets/img1.jpg": "7761b9057c68bca1a978a7897b43a186",
+"assets/assets/img2.jpg": "b39ee79dcd18a3614b6681852c54dc60",
 "assets/assets/vinay.jpg": "de5fceb3e6bec0d35f1fa6455f9ed587",
 "assets/assets/vinay_visiting_card.jpg": "8cedec26a11af519684a6a95ccb66dac",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "97ed99539182e959249d580eb1320519
 "canvaskit/skwasm.worker.js": "bfb704a6c714a75da9ef320991e88b03",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
-"flutter_bootstrap.js": "89d530044f87bb6a226a75b17cc54805",
+"flutter_bootstrap.js": "8c035a2785e4cf02b71cd24cd391efe7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "index.html": "dff13407a18162014073d35928d81758",
 "/": "dff13407a18162014073d35928d81758",
-"main.dart.js": "8f5b847e12d9b366b87057dcc5878ea9",
+"main.dart.js": "5392756f15cf4fac0ea757f7fdf73ef0",
 "manifest.json": "6f55beb699788ce52b6b3e4d009bf9c2",
 "version.json": "c9c71145aca75367910bdf00471f9b03"};
 // The application shell files that are downloaded before a service worker can
